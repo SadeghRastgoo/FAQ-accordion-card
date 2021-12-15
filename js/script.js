e1de859dd9a0b9e15c79b8d55b23ff233bed2103
Sadegh Rastgoo <sadegh.rsg@gmail.com>
2021-12-15 12:37:40 +0330

    accordion functionality

--- js/script.js
+++ js/script.js
@@ -2,22 +2,14 @@
 
 const accordionTitle = document.querySelectorAll(".item__title");
 
-// console.log(accordionTitle);
-
-// const toggleItem = function () {
-//   console.log(document.querySelector(".item__title").parentElement.className);
-//   document
-//     .querySelector(".item__title")
-//     .parentElement.classList.toggle("active-item");
-// };
-
-const toggleItem = function () {
-  console.log(document.querySelector(".item__title").parentElement.className);
-  document
-    .querySelector(".item__title")
-    .parentElement.classList.toggle("active-item");
+let status;
+const showItem = function () {
+  for (let i = 0; i < accordionTitle.length; i++) {
+    accordionTitle[i].parentElement.classList.remove("active-item");
+  }
+  this.parentElement.classList.toggle("active-item");
 };
 
 for (let i = 0; i < accordionTitle.length; i++) {
-  accordionTitle[i].addEventListener("click", toggleItem);
+  accordionTitle[i].addEventListener("click", showItem);
 }
